ea8ba85221b6b43d7119856d8568e1d815651a94
Noah Gift <noah.gift@gmail.com>
2022-03-21 23:16:13 +0000

    adding tf serve via mlflow

--- tf-predict-mlserve.sh
+++ tf-predict-mlserve.sh
@@ -1,3 +1,7 @@
+
+# Check out the docs for this input: 
+# https://docs.databricks.com/applications/mlflow/model-serving.html 
+
 curl http://127.0.0.1:5000/invocations -H 'Content-Type: application/json' -d '{
-     "inputs": [[ 0.0575371   0.50570342 -0.09188722 -0.07555557  0.03215254  0.15547057 -0.74992243 0.80336201]]
-}'
+          "inputs": [[ 0.0575371, 0.50570342, -0.09188722, -0.07555557, 0.03215254, 0.15547057, -0.74992243,0.80336201]]
+     }'
