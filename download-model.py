ff4a33ae57380a1a205835a7b7e185f33f6c2b5c
Noah Gift <noah.gift@gmail.com>
2022-03-21 11:21:10 +0000

    adding new model

--- download-model.py
+++ download-model.py
@@ -2,7 +2,7 @@ from mlflow.store.artifact.models_artifact_repo import ModelsArtifactRepository
 from mlflow.tracking import MlflowClient
 
 client = MlflowClient()
-my_model = client.download_artifacts("51c0348482e042ea8e4b7983ab6bff99", path="model")
+my_model = client.download_artifacts("68baff0203344dfebe89a6c73c6d6cfe", path="model")
 print(f"Placed model in: {my_model}")
 
 
